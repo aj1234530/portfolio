5cf3ff94d3094ab78e4590fbda711effe9568681
mainabhihun <youmailmejust123@gmail.com>
2025-08-12 09:03:31 +0530

    Feat:Added Blog route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blogs from "./pages/blogs";
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
